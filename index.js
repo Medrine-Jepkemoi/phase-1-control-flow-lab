c025a1288f61253b27c21ab2295beb409ebea418
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-16 20:45:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rides){
   // Write your code here!
+if(rides <= 400){
+
+  return `This one is on me!`;
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (rides > 2000 && rides < 2500){
+
+  return `I will gladly take your thirty bucks.`;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (rides > 2500){
+
+  return `No can do.`;
 }
+}
+
+console.log(scuberGreetingForFeet(199))
+console.log(scuberGreetingForFeet(2001))
+console.log(scuberGreetingForFeet(2501))
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+  return city === 'NYC' ? `Ok, sounds good.` : `No go.`;
+  
+}
+
+// console.log(ternaryCheckCity('NYC'))
+console.log(ternaryCheckCity('Pittsburgh'))
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+  switch(tip){
+    case tip = 'generous':
+      return 'Thank you so much.';
+      case tip = 'not as generous':
+        return 'Thank you.'
+        default:
+          return 'Bye.'
+      
+  }
+}
+
+console.log(switchOnCharmFromTip('generous'))
+console.log(switchOnCharmFromTip('not as generous'))
+console.log(switchOnCharmFromTip('thanks for everything'))
